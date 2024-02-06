94defd4b680b7c520af9acd128c61b7aa48988ae
davidpjetraj <118452149+davidpjetraj@users.noreply.github.com>
2024-02-06 21:57:23 +0100

    login

--- login.php
+++ login.php
@@ -85,131 +85,78 @@ include 'constants/check-login.php';
 
 </script>
 </head>
-
-
 <body class="not-transparent-header">
-
 	<div class="container-wrapper">
-
-
 		<header id="header">
-
-
 			<nav class="navbar navbar-default navbar-fixed-top navbar-sticky-function">
-
-				<div class="container">
-					
+				<div class="container">					
 					<div class="logo-wrapper">
 						<div class="logo">
 							<a href="./"><img src="images/main_logo.png" alt="Logo" /></a>
 						</div>
-					</div>
-					
-					<div id="navbar" class="navbar-nav-wrapper navbar-arrow">
-					
-						<ul class="nav navbar-nav" id="responsive-menu">
-						
-							<li>
-							
-								<a href="./">Home</a>
-								
-							</li>
-							
+					</div>					
+					<div id="navbar" class="navbar-nav-wrapper navbar-arrow">					
+						<ul class="nav navbar-nav" id="responsive-menu">					
+							<li>							
+								<a href="./">Home</a>								
+							</li>							
 							<li>
 								<a href="job-list.php">Job List</a>
-
-							</li>
-							
+							</li>							
 							<li>
 								<a href="recruiters.php">recruiters</a>
-							</li>
-							
+							</li>							
 							<li>
 								<a href="applicants.php">applicants</a>
-							</li>
-							
+							</li>							
 							<li>
 								<a href="contact.php">Contact Us</a>
 							</li>
-
-						</ul>
-				
+						</ul>			
 					</div>
-
 					<div class="nav-mini-wrapper">
 						<ul class="nav-mini sign-in">
 							<li><a  href="login.php">login</a></li>
 							<li><a data-toggle="modal" href="#registerModal">register</a></li>
 						</ul>
-					</div>
-				
-				</div>
-				
-				<div id="slicknav-mobile"></div>
-				
-			</nav>
-	
-			<div id="registerModal" class="modal fade login-box-wrapper" tabindex="-1" style="display: none;" data-backdrop="static" data-keyboard="false" data-replace="true">
-			
+					</div>				
+				</div>				
+				<div id="slicknav-mobile"></div>				
+			</nav>	
+			<div id="registerModal" class="modal fade login-box-wrapper" tabindex="-1" style="display: none;" data-backdrop="static" data-keyboard="false" data-replace="true">			
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 					<h4 class="modal-title text-center">Create your account for free</h4>
-				</div>
-				
-				<div class="modal-body">
-				
-					<div class="row gap-20">
-					
+				</div>				
+				<div class="modal-body">				
+					<div class="row gap-20">					
 						<div class="col-sm-6 col-md-6">
 							<a href="register.php?p=Employer" class="btn btn-facebook btn-block mb-5-xs">Register as Recruiter</a>
 						</div>
 						<div class="col-sm-6 col-md-6">
 							<a href="register.php?p=Employee" class="btn btn-facebook btn-block mb-5-xs">Register as Applicant</a>
 						</div>
-
-					</div>
-				
-				</div>
-				
+					</div>				
+				</div>				
 				<div class="modal-footer text-center">
 					<button type="button" data-dismiss="modal" class="btn btn-primary btn-inverse">Close</button>
-				</div>
-				
+				</div>				
 			</div>
-
-
-
-			
 		</header>
-
-
 		<div class="main-wrapper">
-
-
-			<div class="breadcrumb-wrapper">
-			
-				<div class="container">
-				
+			<div class="breadcrumb-wrapper">		
+				<div class="container">				
 					<ol class="breadcrumb-list">
 						<li><a href="./">Home</a></li>
 						<li><span>Access your account</span></li>
-					</ol>
-					
-				</div>
-				
+					</ol>					
+				</div>				
 			</div>
-
-
-			<div class="login-container-wrapper">	
-	
-				<div class="container">
-				
-					<div class="row">
-					
-						<div class="col-md-10 col-md-offset-1">
-						
+			<div class="login-container-wrapper">		
+				<div class="container">				
+					<div class="row">					
+						<div class="col-md-10 col-md-offset-1">						
 							<div class="row">
-
 								<div class="col-sm-6 col-sm-offset-3">
                                 <?php
 								include 'constants/check_reply.php';	
